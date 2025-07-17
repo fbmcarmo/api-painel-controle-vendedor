2e84db1a661f2a78d1d31e2e635ff0cccffe4da0
fbmcarmo <fbrunomcarmo@gmail.com>
2025-07-17 15:35:56 -0300

    Adicionei o index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 6579;
+require('./src/models')
+const produtosRoutes = require('./src/routes/produtos')
+const usersRoutes = require('./src/routes/users')
+const authRoutes = require('./src/routes/auth')
+const cors = require('cors')
+
+
+
+app.use(cors());
+app.use(express.json());
+app.use(produtosRoutes);
+app.use(authRoutes);
+app.use(usersRoutes)
+
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`)
+})
